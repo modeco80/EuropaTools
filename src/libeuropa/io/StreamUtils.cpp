dcb73d826da8ab453f2475c401b8d48a3dfa8554
modeco80 <lily.modeco80@protonmail.ch>
2025-08-02 22:24:20 -0400

    libeuropa/io: Remove iostream streamutils overloads
    They are no longer needed.

--- src/libeuropa/io/StreamUtils.cpp
+++ src/libeuropa/io/StreamUtils.cpp
@@ -9,26 +9,13 @@
 #include "StreamUtils.h"
 
 #include <cstdint>
+#include <europa/base/VirtualFileSystem.hpp>
 #include <stdexcept>
-
-#include "europa/base/VirtualFileSystem.hpp"
 
 namespace europa::io::impl {
 
 	namespace detail {
 
-		void ReadStreamTypeImpl(std::istream& is, char* buffer, std::size_t size) {
-			if(!is)
-				throw std::runtime_error("stream is bad");
-
-			is.read(&buffer[0], size);
-		}
-
-		void WriteStreamTypeImpl(std::ostream& os, const char* buffer, std::size_t buffer_size) {
-			os.write(&buffer[0], buffer_size);
-		}
-
-		// for vfs
 		void ReadStreamTypeImpl(base::VfsFileHandle& is, char* buffer, std::size_t buffer_size) {
 			if(auto n = is->Read(reinterpret_cast<std::uint8_t*>(buffer), buffer_size); n != buffer_size) {
 				throw std::runtime_error("incomplete read");
@@ -42,69 +29,6 @@ namespace europa::io::impl {
 		}
 
 	} // namespace detail
-
-	std::string ReadZeroTerminatedString(std::istream& is) {
-		std::string s;
-		char c;
-
-		if(!is)
-			return "";
-
-		while(true) {
-			c = static_cast<char>(is.get());
-
-			if(c == '\0')
-				return s;
-
-			s.push_back(c);
-		}
-	}
-
-	std::string ReadPString(std::istream& is) {
-		std::string s;
-
-		if(!is)
-			return "";
-
-		std::uint32_t length = static_cast<std::uint32_t>(is.get());
-
-		if(length == 0) {
-			// TODO: is this even possible/valid?
-			static_cast<void>(is.get());
-			return "";
-		}
-
-		// Read the string.
-		s.resize(length - 1);
-		is.read(&s[0], length - 1);
-		static_cast<void>(is.get());
-
-		return s;
-	}
-
-	void WritePString(std::ostream& os, const std::string& string) {
-		if(string.length() >= 255)
-			throw std::invalid_argument("String length too long for WritePString()");
-
-		auto len = static_cast<std::uint8_t>(string.length() + 1);
-
-		// Write the length and the string.
-		os.write(reinterpret_cast<char*>(&len), sizeof(len));
-
-		// It might be iffy to just depend on the C++ STL's
-		// implicit NUL termination?
-		// I mean, c_str() would be broken and the library implementation could be considered
-		// faulty, so idk.
-		os.write(string.data(), string.length() + 1);
-	}
-
-	void TeeInOut(std::istream& is, std::ostream& os) {
-		std::uint8_t buffer[1024] {};
-		while(!is.eof()) {
-			is.read(reinterpret_cast<char*>(&buffer[0]), sizeof(buffer));
-			os.write(reinterpret_cast<char*>(&buffer[0]), is.gcount());
-		}
-	}
 
 	std::string ReadZeroTerminatedString(base::VfsFileHandle& is) {
 		std::string s;
--- src/libeuropa/io/StreamUtils.h
+++ src/libeuropa/io/StreamUtils.h
@@ -10,60 +10,14 @@
 #define EUROPA_TOOLS_STREAMUTILS_H
 
 #include <europa/base/VirtualFileSystem.hpp>
-#include <iostream>
 #include <string>
-
-// This file is really messy. It will be this way for a bit since we're moving away from
-// iostreams in the libeuropa codebase.
 
 namespace europa::io::impl {
 
 	namespace detail {
-		void ReadStreamTypeImpl(std::istream& is, char* buffer, std::size_t size);
-		void WriteStreamTypeImpl(std::ostream& os, const char* buffer, std::size_t buffer_size);
-
 		void ReadStreamTypeImpl(base::VfsFileHandle& is, char* buffer, std::size_t size);
 		void WriteStreamTypeImpl(base::VfsFileHandle& os, const char* buffer, std::size_t buffer_size);
 	} // namespace detail
-
-	// This is lame. But it works :)
-	template <class T>
-	constexpr T ReadStreamType(std::istream& is) {
-		T object {};
-
-		// Absolutely UB.
-		union Hack {
-			T* t;
-			char* c;
-		} address {
-			.t = &object
-		};
-
-		detail::ReadStreamTypeImpl(is, address.c, sizeof(T));
-
-		return object;
-	}
-
-	template <class T>
-	constexpr void WriteStreamType(std::ostream& os, const T& object) {
-		// Absolutely UB.
-		union Hack {
-			const T* t;
-			const char* c;
-		} address {
-			.t = &object
-		};
-
-		detail::WriteStreamTypeImpl(os, address.c, sizeof(T));
-	}
-
-	std::string ReadZeroTerminatedString(std::istream& is);
-	std::string ReadPString(std::istream& is);
-
-	void WritePString(std::ostream& os, const std::string& string);
-
-	/// Tees a input stream to an output stream until the input stream signals EOF.
-	void TeeInOut(std::istream& is, std::ostream& os);
 
 	// This is lame. But it works :)
 	template <class T>
